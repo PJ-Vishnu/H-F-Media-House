6bfe7fc00656338f53a39508bffb2368dbf59695
PJ Vishnu <viztheblaze@gmail.com>
2025-08-26 19:13:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -141,7 +141,7 @@ export const db = {
     const count = await db.collection('portfolio').countDocuments();
     const newItem = { ...item, order: count + 1 };
     const result = await db.collection('portfolio').insertOne(newItem);
-    return mapDoc({ ...newItem, _id: result.insertedId });
+    return mapDoc({ ...newImage, _id: result.insertedId });
   },
   deletePortfolioItem: async (id: string): Promise<{ success: boolean }> => {
     const db = await connectToDb();
