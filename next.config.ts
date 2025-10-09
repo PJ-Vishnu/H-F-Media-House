dd5f6efebd009bd7a849985e662675ffd0955e35
PJ Vishnu <viztheblaze@gmail.com>
2025-10-09 18:01:06 +0000

    Now the hero section images are correct but not the rest

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/uploads/:path*',
-        destination: '/:path*',
+        destination: '/uploads/:path*',
       },
     ]
   },
