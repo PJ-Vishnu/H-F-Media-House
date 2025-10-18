3aac5db3e4a17630b3398e34e1896e25ae78fc5e
PJ Vishnu <viztheblaze@gmail.com>
2025-10-18 16:46:26 +0000

    There still seem to be some image issue in the website

---
+++ home/ubuntu/H-F-Media-House/server.js
@@ -0,0 +1,39 @@
+
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = process.env.PORT || 9002;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+const uploadsDir = path.join(__dirname, 'public', 'uploads');
+const serveUploads = serveStatic(uploadsDir);
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    // Check if the request is for an uploaded file
+    if (pathname.startsWith('/uploads/')) {
+      // Modify the request URL to remove the /uploads prefix for serve-static
+      req.url = req.url.replace('/uploads', '');
+      return serveUploads(req, res, () => {
+        // If serve-static doesn't find a file, let Next.js handle it
+        // This will result in a 404 if the file truly doesn't exist
+        handle(req, res, parsedUrl);
+      });
+    }
+
+    // Let Next.js handle all other requests
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
