c4d0ae71da2242ee285fab510c8813d67ecb2c6d
PJ Vishnu <viztheblaze@gmail.com>
2025-10-18 16:53:50 +0000

    the issue of image not displayed remains on the public facing side

--- home/ubuntu/H-F-Media-House/next.config.ts
+++ home/ubuntu/H-F-Media-House/next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
        {
         protocol: 'http',
         hostname: 'localhost',
-        port: '3000',
+        port: process.env.PORT || '3000',
         pathname: '/**',
       },
     ],
